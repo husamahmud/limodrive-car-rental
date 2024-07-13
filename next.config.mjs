1e3ac655702d459dce375a9f8afb673f47118d4d
husamahmud <husamahmud@gmail.com>
2024-07-14 01:45:11 +0300

    feat: Init `Next` app with `shadcn`

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@lib': path.resolve(__dirname, 'lib'),
+    }
+    return config
+  },
+}
+
+export default nextConfig
---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import type { Metadata } from 'next'
+import { Inter as FontSans } from 'next/font/google'
+
+import './globals.css'
+import { cn } from '@/lib/utils'
+
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+})
+
+export const metadata: Metadata = {
+  title: 'Limodrive',
+  description: 'Limodrive is a car rental service that provides a wide range of vehicles for rent.',
+}
+
+export default function RootLayout({ children }: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+    <body className={cn('min-h-screen font-sans antialiased', fontSans.variable)}>
+    {children}
+    </body>
+    </html>
+  )
+}
